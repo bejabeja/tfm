b5ad8e1fea4f8bf8c00aa683cc273d331c6767aa
bejabeja <miriam.abella211@gmail.com>
2025-04-11 10:04:31 +0200

    add middleware to protect routes

---
+++ api/src/middlewares/authenticate.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req, res, next) => {
+    const token = req.cookies.access_token;
+    req.session = { user: null };
+    try {
+        const data = jwt.verify(token, process.env.JWT_SECRET);
+        req.session.user = data;
+    } catch (error) {
+        return res.status(403).json({ error: 'Unauthorized, invalid token' });
+    }
+
+    next();
+}
