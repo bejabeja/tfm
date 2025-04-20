ca9e33e7281fca1011b301293752366328859aa2
bejabeja <miriam.abella211@gmail.com>
2025-04-20 16:44:45 +0200

    testing prod

--- api/src/controllers/itinerariesController.js
+++ api/src/controllers/itinerariesController.js
@@ -7,6 +7,7 @@ export class ItinerariesController {
         try {
             const userId = req.params.userId;
             const itineraries = await this.itinerariesService.getItinerariesByUserId(userId);
+            console.log(itineraries);
             res.status(200).json(itineraries);
         } catch (error) {
             next(error);
