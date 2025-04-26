39f556c790e2bcbc0daf4ae738bdfafba042fbcf
bejabeja <miriam.abella211@gmail.com>
2025-04-26 13:11:59 +0200

    gg

--- client/src/pages/PrivateLayout.jsx
+++ client/src/pages/PrivateLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-import "../components/LoadingSpinner.scss";
 
 const PrivateLayout = () => {
   const { isAuthenticated, loading } = useSelector((state) => state.auth);
