255db9d1fd00ba507200bee00e4b65583ef55809
bejabeja <miriam.abella211@gmail.com>
2025-05-01 10:36:48 +0200

    styling

--- api/src/models/itinerary.js
+++ api/src/models/itinerary.js
@@ -84,7 +84,7 @@ export class Itinerary {
         const start = new Date(this.startDate);
         const end = new Date(this.endDate);
         const diffTime = Math.abs(end - start);
-        return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+        return Math.ceil(diffTime / (1000 * 60 * 60 * 24)) || 1;
     }
 
     getPlaceholderImage() {
