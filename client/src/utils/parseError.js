e7362f92abbf93fd7e15d27f70fb3f3964fa646d
bejabeja <miriam.abella211@gmail.com>
2025-05-08 16:15:21 +0200

    throw error

--- client/src/utils/parseError.js
+++ client/src/utils/parseError.js
@@ -5,6 +5,6 @@ export const parseError = async (response, defaultMsg) => {
         const data = await response.json();
         msg = data?.error || msg;
     } catch (_) { }
-    // throw new Error(msg);
-    return msg;
+    throw new Error(msg);
+    // return msg;
 }
