0b4935e3066a91bad2488b8e7c1f02f786fe75b7
bejabeja <miriam.abella211@gmail.com>
2025-05-18 15:07:40 +0200

    show real cooordinates in map

--- client/src/components/itineraries/map/Map.jsx
+++ client/src/components/itineraries/map/Map.jsx
@@ -1,7 +1,16 @@
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./Map.scss";
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 
 const Map = ({ location }) => {
   const coordinates = [location.lat, location.lon];
