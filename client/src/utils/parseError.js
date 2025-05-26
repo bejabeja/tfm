f1579c950011534c4384d87c7ce161c10bd0f49d
bejabeja <miriam.abella211@gmail.com>
2025-05-26 14:27:19 +0200

    improve error

--- client/src/utils/parseError.js
+++ client/src/utils/parseError.js
@@ -1,5 +1,10 @@
-export const parseError = async (response, fallbackMessage = "Something went wrong") => {
-  const data = await response.json().catch(() => ({}));
-  const message = data.message || fallbackMessage;
-  throw new Error(message);
-};
+export const parseError = async (response, defaultMsg = "Something went wrong") => {
+  let msg = defaultMsg;
+  try {
+
+    const data = await response.json();
+    msg = data?.error || msg;
+  } catch (_) { }
+  throw new Error(msg);
+  // return msg;
+}
