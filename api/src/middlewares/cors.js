93847260db5aabc4aec9560764aed7f17a6ddac0
bejabeja <miriam.abella211@gmail.com>
2025-06-01 17:16:13 +0200

    testing cors

--- api/src/middlewares/cors.js
+++ api/src/middlewares/cors.js
@@ -3,15 +3,19 @@ import config from '../config/config.js';
 
 const ACCEPTED_ORIGINS = [config.originOne];
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+    // cors({
+    //     origin: (origin, callback) => {
+    //         if (acceptedOrigins.includes(origin)) {
+    //             return callback(null, true);
+    //         }
+    //         if (!origin) {
+    //             return callback(null, true);
+    //         }
+    //         return callback(new Error('CORS policy error'));
+    //     },
+    //     credentials: true,
+    // });
     cors({
-        origin: (origin, callback) => {
-            if (acceptedOrigins.includes(origin)) {
-                return callback(null, true);
-            }
-            if (!origin) {
-                return callback(null, true);
-            }
-            return callback(new Error('CORS policy error'));
-        },
+        origin: true,
         credentials: true,
     });
