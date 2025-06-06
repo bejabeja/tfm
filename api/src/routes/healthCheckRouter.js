e7f7b298f037a4eb469347bfd5cdca1559d3cd48
bejabeja <miriam.abella211@gmail.com>
2025-06-06 09:48:35 +0200

    export route

--- api/src/routes/healthCheckRouter.js
+++ api/src/routes/healthCheckRouter.js
@@ -1,16 +1,17 @@
-import express from 'express';
-import client from './db/clientPostgres.js';
+import { Router } from "express";
+import client from '../db/clientPostgres.js';
 
-const router = express.Router();
+export const healthCheckRouter = () => {
+    const router = Router();
+    router.get('/health-check', async (req, res) => {
+        try {
+            await client.query('SELECT 1');
+            res.status(200).json({ status: 'ok', message: 'DB connection is healthy' });
+        } catch (error) {
+            console.error('Health check failed:', error);
+            res.status(500).json({ status: 'error', message: 'DB connection failed' });
+        }
+    });
 
-router.get('/health-check', async (req, res) => {
-    try {
-        await client.query('SELECT 1');
-        res.status(200).json({ status: 'ok', message: 'DB connection is healthy' });
-    } catch (error) {
-        console.error('Health check failed:', error);
-        res.status(500).json({ status: 'error', message: 'DB connection failed' });
-    }
-});
-
-export default router;
+    return router;
+};
